05b8963c0485e856303b3e7b17395686b0cc0d57
Sai Siddhartha Kondamu <kondamu.s@northeastern.edu>
2022-04-29 18:08:35 -0400

    made changes in BusinessModel

--- HospitalEcosystemManagement/src/BusinessModel/User/User.java
+++ HospitalEcosystemManagement/src/BusinessModel/User/User.java
@@ -5,6 +5,7 @@
 package BusinessModel.User;
 
 import BusinessModel.Employee.Employee;
+import BusinessModel.Roles.Role;
 
 /**
  *
@@ -15,6 +16,50 @@ public class User {
     private String username;
     private String password;
     private Employee employee;
-    //private Role role;
+    private Role role;
+
+    public User() {
+    }
+
+    public User(String username, String password, Employee employee, Role role) {
+        this.username = username;
+        this.password = password;
+        this.employee = employee;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    
+    
     
 }
