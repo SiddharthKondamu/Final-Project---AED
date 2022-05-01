a70563c4f7074e88905091580dbec0021edd45ed
Kumari Puja <puja.k@northeastern.edu>
2022-04-30 22:11:59 -0400

    Backend code for police

--- HospitalEcosystemManagement/src/BusinessModel/PoliceDepartment/PoliceDepartment.java
+++ HospitalEcosystemManagement/src/BusinessModel/PoliceDepartment/PoliceDepartment.java
@@ -4,10 +4,65 @@
  */
 package BusinessModel.PoliceDepartment;
 
+import BusinessModel.Patient.Patient;
+import java.util.ArrayList;
+
 /**
  *
  * @author siddh
  */
 public class PoliceDepartment {
+    private String departmentName;
+    private String userName;
+    private String address;
+    private ArrayList<Patient> policeRecordsList;
+
+    public PoliceDepartment() {
+        policeRecordsList = new ArrayList<>();
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public ArrayList<Patient> getPoliceRecordsList() {
+        return policeRecordsList;
+    }
+
+    public void setPoliceRecordsList(ArrayList<Patient> policeRecordsList) {
+        this.policeRecordsList = policeRecordsList;
+    }
+    public void addtoRecord(Patient patient)
+    {
+      /*  for(Patient p:labRecordsList){
+        if(!labRecordsList.contains(patient)){
+        labRecordsList.add(patient);
+        }
+        else if(p==patient && p.getPharmaStatus().equals("Delivered")){
+        labRecordsList.add(p);
+        }
+        }*/
+        policeRecordsList.add(patient);
+    }
     
 }
